1d2de03256b8e7ac121adc3a73da0579afc636ad
emma-boamah <emmanuelboamah046@gmail.com>
2024-12-06 04:52:44 -0800

    Added case, DELETE, and PUT

--- PHP_REST_API/api.php
+++ PHP_REST_API/api.php
@@ -15,7 +15,8 @@ switch( $method ) {
             $stmt->bindParam(1, $id, PDO::PARAM_INT);
             $stmt->execute();
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            
+            $stmt = null;
+
             echo json_encode($result);
         } else{
             $query = ('SELECT * FROM profiles');
@@ -26,6 +27,7 @@ switch( $method ) {
             while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
                 $users[] = $row;
             }
+            $stmt = null;
             echo json_encode($users);
         }
         break;
@@ -35,10 +37,49 @@ switch( $method ) {
         $age = $input['age'];
 
         // INSERT DATA INTO THE DATABASE
-        $query = ('INSERT INTO people (Name, E_mail, Age) VALUES(?, ?, ?)');
+        $query = ('INSERT INTO profiles (Name, E_mail, Age) VALUES(?, ?, ?)');
         $stmt = $db_Connection->prepare($query);
         $stmt->bindParam(1, $name, PDO::PARAM_STR);
         $stmt->bindParam(1, $email, PDO::PARAM_STR);
         $stmt->bindParam(1, $age, PDO::PARAM_INT);
+        $stmt->execute();
+        $stmt = null;
 
-}   
+        echo json_encode(["message" => "user added successfully"]);
+        break;
+
+    case "PUT":
+        $id = $_GET['id'];
+        $name = $input['name'];
+        $email = $input['email'];
+        $age = $input['age'];
+        
+        $query = ("UPDATE profiles SET name=?, email=?, age=?, WHERE id=?");
+        $stmt = $db_Connection->prepare($query);
+        $stmt->bindParam(1, $name, PDO::PARAM_STR);
+        $stmt->bindParam(1, $email, PDO::PARAM_STR);
+        $stmt->bindParam(1, $age, PDO::PARAM_STR);
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $stmt = null;
+        echo json_encode(["message" => "user updated successfully"]);
+        break;
+
+    case "DELETE":
+        $id = $_GET["id"];
+        $query = ("DELETE FROM profiles WHERE id=?");
+        $stmt = $db_Connection->prepare($query);
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $stmt = null;
+        echo json_encode(["message" => "user deleted successfully"]);
+        break;
+    
+    default:
+        echo json_encode(["message" => "Invalid request method"]);
+        break;
+
+    
+} 
+
+$db_Connection = null;
